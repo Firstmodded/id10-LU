6fa5b6040c8972166ff6f2a21277a50a2b53aa1d
Slyfer88 <ma3485932@gmail.com>
2025-02-08 17:18:13 +0200

    todo-list

--- id10-LU/js/dashboard.js
+++ id10-LU/js/dashboard.js
@@ -10,3 +10,45 @@ document.getElementById('toggle-btn').addEventListener('click', function() {
     var navbar = document.getElementById('navbar');
     navbar.classList.toggle('open');
 });
+
+/*TODO List*/
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-c1");
+
+function addTask(){
+    if(inputBox.value == ''){
+        alert("You must write somthing!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("todo-span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "TODO-SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+localStorage.clear();
