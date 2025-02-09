1f5e186b4b0192fd4f2cade7b815e1ac6854714a
Slyfer88 <ma3485932@gmail.com>
2025-02-09 22:47:15 +0200

    major-changes

--- id10-LU/js/dashboard.js
+++ id10-LU/js/dashboard.js
@@ -99,73 +99,48 @@ showTask();
 
 //Pomodoro Timer
 
+const start = document.getElementById("start");
+const stopp = document.getElementById("stop");
+const reset = document.getElementById("reset");
+const timer = document.getElementById("timer");
 
-// variables
+let timeLeft = 1500;
+let interval;
 
-let workTittle = document.getElementById('work');
-let breakTittle = document.getElementById('break');
 
-let workTime = 25;
-let breakTime = 5;
+const updateTimer = () => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
 
-let seconds = "00"
-
-// display
-window.onload = () => {
-    document.getElementById('minutes').innerHTML = workTime;
-    document.getElementById('seconds').innerHTML = seconds;
-
-    workTittle.classList.add('active');
+    timer.innerHTML =
+    `${minutes.toString().padStart(2,"0")}
+    :
+    ${seconds.toString().padStart(2,"0")}`;
 }
 
-// start timer
-function start() {
-    // change button
-    document.getElementById('start').style.display = "none";
-    document.getElementById('reset').style.display = "block";
+const startTimer = () => {
+    interval = setInterval(() => {
+        timeLeft--;
+        updateTimer();
 
-    // change the time
-    seconds = 59;
-
-    let workMinutes = workTime - 1;
-    let breakMinutes = breakTime - 1;
-
-    breakCount = 0;
-
-    // countdown
-    let timerFunction = () => {
-        //change the display
-        document.getElementById('minutes').innerHTML = workMinutes;
-        document.getElementById('seconds').innerHTML = seconds;
-
-        // start
-        seconds = seconds - 1;
-
-        if(seconds === 0) {
-            workMinutes = workMinutes - 1;
-            if(workMinutes === -1 ){
-                if(breakCount % 2 === 0) {
-                    // start break
-                    workMinutes = breakMinutes;
-                    breakCount++
-
-                    // change the painel
-                    workTittle.classList.remove('active');
-                    breakTittle.classList.add('active');
-                }else {
-                    // continue work
-                    workMinutes = workTime;
-                    breakCount++
-
-                    // change the painel
-                    breakTittle.classList.remove('active');
-                    workTittle.classList.add('active');
-                }
-            }
-            seconds = 59;
+        if(timeLeft === 0 ){
+            clearInterval(interval);
+            alert("Times Up!");
+            timeLeft = 1500;
+            updateTimer();
         }
-    }
+    },
+    1000)
+};
 
-    // start countdown
-    setInterval(timerFunction, 1000); // 1000 = 1s
+const stopTimer = () => clearInterval(interval);
+
+const resetTimer = () => {
+    clearInterval(interval);
+    timeLeft = 1500;
+    updateTimer();
 }
+
+start.addEventListener("click",startTimer);
+stopp.addEventListener("click",stopTimer);
+reset.addEventListener("click",resetTimer);
