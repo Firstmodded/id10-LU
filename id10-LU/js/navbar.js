c9fb142cb8bf62c5898a900ea800d6a6982bce20
Slyfer88 <ma3485932@gmail.com>
2025-02-11 18:02:28 +0200

    navbar-fixing

--- id10-LU/js/navbar.js
+++ id10-LU/js/navbar.js
@@ -1,22 +1,42 @@
-/*const toggleBtn = document.getElementById('toggle-btn')
+const toggleBtn = document.getElementById('toggle-btn')
 const navbar = document.getElementById('navbar')
 
-function toggleSideBar(){
+function toggleNavBar(){
     navbar.classList.toggle('close')
     toggleBtn.classList.toggle('rotate')
+
+   closeAllSubMenus();
 }
 
 document.getElementById('toggle-btn').addEventListener('click', function() {
     var navbar = document.getElementById('navbar');
     navbar.classList.toggle('open');
 });
-*/
+
 
 
 
 /*Sub-menu*/
 
 function toggleSubMenu(button){
+
+    if(!button.nextElementSibling.classList.contains('show')){
+        closeAllSubMenus();
+    }
+   
+    
     button.nextElementSibling.classList.toggle('show');
     button.classList.toggle('rotate');
+
+    if(navbar.classList.contains('close')){
+        navbar.classList.toggle('close');
+        toggleBtn.classList.toggle('rotate');
+    }
+}
+
+function closeAllSubMenus(){
+    Array.from(navbar.getElementsByClassName('show')).forEach(ul => {
+        ul.classList.remove('show');
+        ul.previousElementSibling.classList.remove('rotate');
+    })
 }
